f334bcceb0b57ffc03350bf791af06c05a8a8049
Park Dongkyu <86235780+dong99u@users.noreply.github.com>
2024-08-28 23:51:53 +0900

    Feature: 회원 정보 가져오기 API 구현

--- src/main/java/com/ghpg/morningbuddies/auth/member/service/MemberQueryService.java
+++ src/main/java/com/ghpg/morningbuddies/auth/member/service/MemberQueryService.java
@@ -3,5 +3,5 @@ package com.ghpg.morningbuddies.auth.member.service;
 import com.ghpg.morningbuddies.auth.member.dto.MemberResponseDto;
 
 public interface MemberQueryService {
-    MemberResponseDto.MemberInfo getMemberInfo();
+    MemberResponseDto.MemberInfo getMemberInfo(String refreshToken);
 }
