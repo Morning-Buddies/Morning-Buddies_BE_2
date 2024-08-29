9140d25926817e7ecdeebde693116558050cee35
Park Dongkyu <86235780+dong99u@users.noreply.github.com>
2024-08-29 14:08:19 +0900

    Refactor: id 추가

--- src/main/java/com/ghpg/morningbuddies/domain/group/dto/GroupRequestDto.java
+++ src/main/java/com/ghpg/morningbuddies/domain/group/dto/GroupRequestDto.java
@@ -1,35 +1,39 @@
 package com.ghpg.morningbuddies.domain.group.dto;
 
-import jakarta.persistence.Lob;
-import jakarta.validation.constraints.*;
-import lombok.Getter;
-
 import java.time.LocalTime;
+
+import jakarta.persistence.Lob;
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+import lombok.Getter;
 
 public class GroupRequestDto {
 
-    @Getter
-    public static class CreateGroupDto{
+	@Getter
+	public static class CreateGroupDto {
 
-        @NotNull
-        private Integer groupId;
+		@NotNull
+		private Integer groupId;
 
-        @NotBlank
-        private String groupName;
+		@NotBlank
+		private String groupName;
 
-        @NotNull
-        private LocalTime wakeUpTime;
+		@NotNull
+		private LocalTime wakeUpTime;
 
-        @Min(value = 1)
-        @Max(value = 10)
-        @NotNull
-        private Integer maxParticipantCount = 1;
+		@Min(value = 1)
+		@Max(value = 10)
+		@NotNull
+		private Integer maxParticipantCount;
 
-        @NotBlank
-        @Size(max = 500)
-        private String description;
+		@NotBlank
+		@Size(max = 500)
+		private String description;
 
-        @Lob
-        private String imageUrl;
-    }
+		@Lob
+		private String imageUrl;
+	}
 }
--- src/main/java/com/ghpg/morningbuddies/domain/group/dto/GroupResponseDto.java
+++ src/main/java/com/ghpg/morningbuddies/domain/group/dto/GroupResponseDto.java
@@ -1,60 +1,64 @@
 package com.ghpg.morningbuddies.domain.group.dto;
 
-import com.ghpg.morningbuddies.auth.member.dto.MemberResponseDto;
-import com.ghpg.morningbuddies.auth.member.entity.Member;
-import lombok.*;
-
 import java.time.LocalTime;
 import java.util.List;
 
+import com.ghpg.morningbuddies.auth.member.dto.MemberResponseDto;
+import com.ghpg.morningbuddies.auth.member.entity.Member;
+
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 public class GroupResponseDto {
 
-    // 그룹 정보 DTO
-    @Getter
-    @Builder
-    @NoArgsConstructor(access = AccessLevel.PROTECTED)
-    @AllArgsConstructor
-    public static class GroupDetailDTO{
-        private Long groupId;
-        private String groupName;
-        private String description;
-        private LocalTime wakeUpTime;
-        private int currentParticipantCount;
-        private int maxParticipantCount;
-        private String imageUrl;
-        private List<MemberResponseDto.MemberSummaryDTO> members;
-        private LeaderDTO leader;
-    }
+	// 그룹 정보 DTO
+	@Getter
+	@Builder
+	@NoArgsConstructor(access = AccessLevel.PROTECTED)
+	@AllArgsConstructor
+	public static class GroupDetailDTO {
+		private Long groupId;
+		private String groupName;
+		private String description;
+		private LocalTime wakeUpTime;
+		private int currentParticipantCount;
+		private int maxParticipantCount;
+		private String imageUrl;
+		private List<MemberResponseDto.MemberSummaryDTO> members;
+		private LeaderDTO leader;
+	}
 
-    // 리더 DTO
-    @Getter
-    @Builder
-    @NoArgsConstructor(access = AccessLevel.PROTECTED)
-    @AllArgsConstructor
-    public static class LeaderDTO {
-        private Long id;
-        private String firstName;
-        private String lastName;
-        private String email;
+	// 리더 DTO
+	@Getter
+	@Builder
+	@NoArgsConstructor(access = AccessLevel.PROTECTED)
+	@AllArgsConstructor
+	public static class LeaderDTO {
+		private Long id;
+		private String firstName;
+		private String lastName;
+		private String email;
 
+		public static LeaderDTO from(Member member) {
+			return LeaderDTO.builder()
+				.id(member.getId())
+				.firstName(member.getFirstName())
+				.lastName(member.getLastName())
+				.email(member.getEmail())
+				.build();
+		}
+	}
 
-        public static LeaderDTO from(Member member) {
-            return LeaderDTO.builder()
-                    .id(member.getId())
-                    .firstName(member.getFirstName())
-                    .lastName(member.getLastName())
-                    .email(member.getEmail())
-                    .build();
-        }
-    }
-
-    @Getter
-    @Builder
-    @NoArgsConstructor(access = AccessLevel.PROTECTED)
-    @AllArgsConstructor
-    public static class GroupInfo {
-        private String name;
-        private LocalTime wakeupTime;
-    }
+	@Getter
+	@Builder
+	@NoArgsConstructor(access = AccessLevel.PROTECTED)
+	@AllArgsConstructor
+	public static class GroupInfo {
+		private Long id;
+		private String name;
+		private LocalTime wakeupTime;
+	}
 }
