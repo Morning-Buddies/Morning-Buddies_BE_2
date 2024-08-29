fbc82958a8ef38735c1375430ccbb56b13545c44
Park Dongkyu <86235780+dong99u@users.noreply.github.com>
2024-08-29 14:27:23 +0900

    Feature: 그룹 만들기 API 완성

--- src/main/java/com/ghpg/morningbuddies/auth/member/repository/MemberRepository.java
+++ src/main/java/com/ghpg/morningbuddies/auth/member/repository/MemberRepository.java
@@ -1,14 +1,17 @@
 package com.ghpg.morningbuddies.auth.member.repository;
 
-import com.ghpg.morningbuddies.auth.member.entity.Member;
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.Optional;
+import com.ghpg.morningbuddies.auth.member.entity.Member;
 
 public interface MemberRepository extends JpaRepository<Member, Long> {
-    Optional<Member> findByEmail(String email);
+	boolean existsByEmail(String email);
 
-    @EntityGraph(attributePaths = {"groups"})
-    Optional<Member> findGroupsByEmail(String email);
+	Optional<Member> findByEmail(String email);
+
+	@EntityGraph(attributePaths = {"groups"})
+	Optional<Member> findGroupsByEmail(String email);
 }
