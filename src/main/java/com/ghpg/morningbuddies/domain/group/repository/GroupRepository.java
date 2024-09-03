c9f02306d4eeabb09669184f5973cbc07eaa60af
taegyunB <taegyunb99@gmail.com>
2024-09-03 16:24:32 +0900

    Feature: 일찍 일어나는 그룹 기준 조회

--- src/main/java/com/ghpg/morningbuddies/domain/group/repository/GroupRepository.java
+++ src/main/java/com/ghpg/morningbuddies/domain/group/repository/GroupRepository.java
@@ -1,5 +1,6 @@
 package com.ghpg.morningbuddies.domain.group.repository;
 
+import java.time.LocalTime;
 import java.util.Optional;
 
 import org.springframework.data.domain.Page;
@@ -9,6 +10,7 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
 import com.ghpg.morningbuddies.domain.group.entity.Groups;
+import org.springframework.web.bind.annotation.RequestParam;
 
 public interface GroupRepository extends JpaRepository<Groups, Long> {
 
@@ -20,4 +22,8 @@ public interface GroupRepository extends JpaRepository<Groups, Long> {
 	// 핫한 그룹 기준
 	@Query("SELECT g FROM Groups g ORDER BY g.successCount DESC")
 	Page<Groups> getHotGroups(Pageable pageable);
+
+	// 일찍 일어나는 그룹 기준
+	@Query("SELECT g FROM Groups g WHERE g.wakeupTime <= :time")
+	Page<Groups> getGroupsByEarlyMorning(@RequestParam("time") LocalTime time, Pageable pageable);
 }
