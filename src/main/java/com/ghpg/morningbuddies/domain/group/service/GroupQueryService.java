bc03a9da3a0f2b86c3f1d47aedb2a54d9c543e1e
taegyunB <taegyunb99@gmail.com>
2024-09-03 13:45:21 +0900

    Fix: 그룹 가입 요청 리스트 GroupQueryService로 이동

--- src/main/java/com/ghpg/morningbuddies/domain/group/service/GroupQueryService.java
+++ src/main/java/com/ghpg/morningbuddies/domain/group/service/GroupQueryService.java
@@ -4,6 +4,8 @@ import org.springframework.data.domain.Pageable;
 
 import com.ghpg.morningbuddies.domain.group.dto.GroupResponseDto;
 
+import java.util.List;
+
 public interface GroupQueryService {
 
 	// 그룹 정보 가져오기
@@ -11,4 +13,8 @@ public interface GroupQueryService {
 
 	// 그룹 검색 결과 가져오기
 	GroupResponseDto.SearchedGroupInfoList getSearchedGroupInfoList(String keyword, Pageable pageable);
+
+	// 그룹 가입 요청 리스트
+	List<GroupResponseDto.JoinRequestDTO> findByGroupAndStatus(Long groupId);
+
 }
