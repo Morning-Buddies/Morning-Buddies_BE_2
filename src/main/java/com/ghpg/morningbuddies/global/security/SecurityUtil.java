cf11c6c2b73524951e92d100ee731da76019ae20
Park Dongkyu <86235780+dong99u@users.noreply.github.com>
2024-09-15 15:50:40 +0900

    Bug: firebase config 수정

--- src/main/java/com/ghpg/morningbuddies/global/security/SecurityUtil.java
+++ src/main/java/com/ghpg/morningbuddies/global/security/SecurityUtil.java
@@ -1,5 +1,8 @@
 package com.ghpg.morningbuddies.global.security;
 
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+
 import com.ghpg.morningbuddies.auth.member.dto.CustomMemberDetails;
 import com.ghpg.morningbuddies.global.exception.common.GeneralException;
 import com.ghpg.morningbuddies.global.exception.common.code.GlobalErrorCode;
