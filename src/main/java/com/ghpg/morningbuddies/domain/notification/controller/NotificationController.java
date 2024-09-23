a33765db77070c79d6b148bafc6dd1f177ca55fe
Park Dongkyu <86235780+dong99u@users.noreply.github.com>
2024-09-23 15:52:46 +0900

    Bug: Cors 처리

---
+++ src/main/java/com/ghpg/morningbuddies/domain/notification/controller/NotificationController.java
@@ -0,0 +1,15 @@
+package com.ghpg.morningbuddies.domain.notification.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/notifications")
+public class NotificationController {
+
+}
