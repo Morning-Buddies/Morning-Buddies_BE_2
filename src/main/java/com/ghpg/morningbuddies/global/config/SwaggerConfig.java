ede23157df38ecd2e7a051810826061bf2f1d31d
Park Dongkyu <86235780+dong99u@users.noreply.github.com>
2024-10-08 17:23:00 +0900

    Feature: Swagger Resetting

--- src/main/java/com/ghpg/morningbuddies/global/config/SwaggerConfig.java
+++ src/main/java/com/ghpg/morningbuddies/global/config/SwaggerConfig.java
@@ -3,8 +3,10 @@ package com.ghpg.morningbuddies.global.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityScheme;
 
 @Configuration
 public class SwaggerConfig {
@@ -12,6 +14,9 @@ public class SwaggerConfig {
 	@Bean
 	public OpenAPI openAPI() {
 		return new OpenAPI()
+			.components(new Components().addSecuritySchemes("bearer-jwt",
+				new SecurityScheme().type(SecurityScheme.Type.HTTP).scheme("bearer").bearerFormat("JWT")
+					.in(SecurityScheme.In.HEADER).name("Authorization")))
 			.info(new Info()
 				.title("Morning Buddies API")
 				.description("Morning Buddies API 명세서")
