306cffea106cb0a42dac0f983d6ac312fe3bbc2f
Park Dongkyu <86235780+dong99u@users.noreply.github.com>
2024-10-14 11:26:17 +0900

    Feature: Open ai key

--- src/main/java/com/ghpg/morningbuddies/global/config/SecurityConfig.java
+++ src/main/java/com/ghpg/morningbuddies/global/config/SecurityConfig.java
@@ -90,6 +90,7 @@ public class SecurityConfig {
 		configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
 		configuration.setAllowCredentials(true);
 		configuration.setAllowedHeaders(Arrays.asList("*"));
+		configuration.setExposedHeaders(Arrays.asList("access"));
 		configuration.setMaxAge(3600L);
 		configuration.setExposedHeaders(Arrays.asList("Authorization"));
 
